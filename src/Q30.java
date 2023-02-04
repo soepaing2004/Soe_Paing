dea6429063fd7cf9e9c1791edbe8ad557fc3a8b3
Soe Paing <soepaing02132004@gmail.com>
2023-02-04 16:24:09 +0630

    change

--- src/Q30.java
+++ src/Q30.java
@@ -8,8 +8,11 @@ public class Q30 {
         int index_number=s.nextInt();
         int array[]=new int[index_number];
         System.out.println("Enter the number of element:");
-        for (int i:array) {
+        for (int i=0;i<index_number;i++){
             array[i]=s.nextInt();
+        }
+        for (int i:array
+             ) {
         }
         System.out.println(Arrays.toString(array));
     }
