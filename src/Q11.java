38fa246a3ab5e3e031ead085de23f88e61448eae
Soe Paing <soepaing02132004@gmail.com>
2023-02-05 13:22:41 +0630

    change

--- src/Q11.java
+++ src/Q11.java
@@ -4,6 +4,6 @@ import java.util.Scanner;
 public class Q11 {
     public static void main(String[] args) {
         int array[]={1,2,3,4,5,6};
-        System.out.println(array);
+        System.out.println(Arrays.toString(array));
     }
 }
